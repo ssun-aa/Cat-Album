b4bc58ee11696e2f9da0dcdf8efd79a2dbce8631
ssun-aa <ssk6200@daum.net>
2022-06-03 03:03:15 +0900

    feat: drag & drop

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,31 +1,70 @@
-import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
-import { useEffect, useState } from 'react';
+import {
+  DragDropContext,
+  Draggable,
+  Droppable,
+  DropResult,
+} from 'react-beautiful-dnd';
 import { useRecoilState } from 'recoil';
 import { favListState } from 'recoil/atom';
 import ImgCard from 'components/Navigation/imgCard';
-import store from 'storejs';
 import styles from './favorites.module.scss';
 
 function Favorites() {
   const [favoriteList, setFavoriteList] =
     useRecoilState<string[]>(favListState);
 
+  const handleOnDragEnd = (result: DropResult) => {
+    if (!result.destination) return;
+
+    const currentList = [...favoriteList];
+    const beforIndex = result.source.index;
+    const afterIndex = result.destination.index;
+
+    const removeItem = currentList.splice(beforIndex, 1);
+
+    currentList.splice(afterIndex, 0, removeItem[0]);
+
+    setFavoriteList(currentList);
+  };
+
   return (
     <div className={styles.wrap}>
       <header className={styles.title}>
         <p>내 즐겨찾기</p>
       </header>
-      <main>
-        {favoriteList.map((item, i) => (
-          <ImgCard
-            width="40%"
-            // eslint-disable-next-line react/no-array-index-key
-            key={i}
-            mainCat={item}
-            alreadyFavorite
-          />
-        ))}
-      </main>
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId="favorites">
+          {(provided) => (
+            <main
+              className="favorites"
+              // eslint-disable-next-line react/jsx-props-no-spreading
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {favoriteList.map((item, i) => (
+                <Draggable key={item} draggableId={item} index={i}>
+                  {(provide) => (
+                    <li
+                      ref={provide.innerRef}
+                      // eslint-disable-next-line react/jsx-props-no-spreading
+                      {...provide.draggableProps}
+                      // eslint-disable-next-line react/jsx-props-no-spreading
+                      {...provide.dragHandleProps}
+                    >
+                      <ImgCard
+                        // eslint-disable-next-line react/no-array-index-key
+                        key={i}
+                        mainCat={item}
+                        alreadyFavorite
+                      />
+                    </li>
+                  )}
+                </Draggable>
+              ))}
+            </main>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
